e89784b32c74d7632d3f9d3ef2fc4794af15aafd
jahnga <edm47@pitt.edu>
2024-12-09 19:40:35 -0500

    add guest storage

--- guest.js
+++ guest.js
@@ -3,18 +3,24 @@
 function saveQuestion(event) {
     event.preventDefault();
 
-    const selectedLanguages = document.querySelector('input[name="languageSelection"]:checked');
+    const selectedLanguage = document.querySelector('input[name="languageSelection"]:checked');
     const selectedLevel = document.querySelector('input[name="programmerLevel"]:checked');
-    console.log(selectedLanguages);
+    console.log(selectedLanguage.value);
 
 
-    if (!selectedLanguages || !selectedLevel) {
+    if (!selectedLanguage || !selectedLevel) {
         alert("Please select language(s) and programming level before submitting.");
         return;  // Exit if no question is selected
     }
 
-    let guestData = {};
-    let languages = [];
+    let guestData = {
+        language: selectedLanguage.value,
+        level: selectedLevel.value,
+    };
+
+    const jsonData = JSON.stringify(guestData);
+
+    localStorage.setItem('guestData', jsonData);
 
     alert("Guest data saved!");
 }
